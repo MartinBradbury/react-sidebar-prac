c3f7542572b4acc4e9740d921dcfdd95fba903a6
Martin Bradbury <martinbradbury@btinternet.com>
2024-04-17 14:52:34 +0000

    added blank href

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -3,9 +3,9 @@ import React from "react";
 const Sidebar = () => {
     return (
         <div>
-            <a>My Photos</a>
-            <a>My Illustrations</a>
-            <a>My Paintings</a>
+            <a href = "#." target= "_blank">My Photos</a>
+            <a href = "#." target= "_blank">My Illustrations</a>
+            <a href = "#." target= "_blank">My Paintings</a>
         </div>
     );
 }
